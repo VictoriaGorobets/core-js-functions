4cc052a91917160bfa942dbaa51f4393cba6fbc7
VictoriaGorobets <vickyhome1996@gmail.com>
2024-04-02 23:25:53 +0300

    feat: solve task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  return 'getCurrentFunctionName';
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,8 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  return func === undefined ? '' : func.toString();
 }
 
 /**
